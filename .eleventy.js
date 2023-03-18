f319e895910a902ac563005b17974a5c4a4b99d2
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-03-18 23:02:59 +0900

    動的にリストを取ってくる

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets")
+  eleventyConfig.addPassthroughCopy("data")
 
   return {
     dir: {
--- assets/test.js
+++ assets/test.js
@@ -1,7 +1,15 @@
 Vue.createApp({
+  mounted() {
+    fetch('../data/list.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data
+      });
+  },
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      items: []
     }
   }
 }).mount('#app')
